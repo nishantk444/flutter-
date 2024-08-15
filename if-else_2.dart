8976524bdc0ffa672f86e750e29639b410215fd5
nishantk444 <kadamnishu55@gmail.com>
2024-08-16 01:33:28 +0530

    if-else_2

---
+++ if-else_2.dart
@@ -0,0 +1,160 @@
+// if-else statement
+// 1
+// void main(){
+//   var x = 4;
+//   if(x == 4){
+//     print("can't run a project");
+//   }else if(x == 80){
+//     print("can run a flutter project");
+//   }
+// }
+
+
+// 2
+// void main(){
+//   var x = 1;
+//   if(x == 1){
+//     print("Voilet");
+//   }else if(x == 2){
+//     print("Indigo");
+//   }else if(x == 3){
+//     print("Blue");
+//   }else if(x == 4){
+//     print("Green");
+//   }else if(x == 5){
+//     print("Yellow");
+//   }else if(x == 6){
+//     print("Orange");
+//   }else if(x == 7){
+//     print("Red");
+//   }
+// }
+
+
+// 3
+// void main(){
+//   var x = 25;
+//   if(x>30 && x<50){
+//     print("Number is in correct range");
+//   }else{
+//     print("Invalid number");
+//   }
+// }
+
+
+// 4
+// void main(){
+//   var x = 20;
+//   if(x>=16 && x%2==0){
+//     print("correct number");
+//   }else{
+//     print("Incorrect number");
+//   }
+// }
+
+
+// 5
+// void main(){
+//   var x = 18;
+//   if(x%3==2){
+//     print("Remainder is equal to 2");
+//   }else if(x%3<2){
+//     print("Remanider is less than 2");
+//   }
+// }
+
+
+// 6
+// void main(){
+//   var x = 22;
+//   if(x<18.5){
+//     print("Underweight");
+//   }else if(x>=18.5 && x<=24.9){
+//     print("Normal");
+//   }else if(x>=25.0 && x<=29.9){
+//     print("Overweight");
+//   }else if(x>=30.0 && x<=34.9){
+//     print("Obese");
+//   }else if(x>=30.0){
+//     print("Extreme Obese");
+//   }
+// }
+
+
+// 7
+// void main(){
+//   var x = 9;
+//   if(x>=8){
+//     print("You cannot enter the lift");
+//   }else{
+//     print("You can enter the lift");
+//   }
+// }
+
+
+// 8
+// void main(){
+//   var vehicle = 'Scooter';
+//   if(vehicle == 'Bike'){
+//     print("Go to parking 2");
+//   }else if(vehicle == 'Scooter'){
+//     print("Go to parking 1");
+//   }
+// }
+
+
+// 9
+// void main(){
+//   var x = 67;
+//   if(x>=0 && x<25){
+//     print("Your Grade D");
+//   }else if(x>=25 && x<50){
+//     print("Your Grade C");
+//   }else if(x>=50 && x<75){
+//     print("Your Grade B");
+//   }else if(x>=75 && x<=100){
+//     print("Your Grade A");
+//   }
+// }
+
+
+// 10
+// void main(){
+//   var percentage = 84;
+//   var cgpa = 9;
+//   if(percentage>=70.0 && cgpa>=7.0){
+//     print("You are eligible");
+//   }else{
+//     print("you are not eligible");
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
