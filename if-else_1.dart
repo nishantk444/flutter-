b6f5e9e31a1beceeeeaffe568da6c14ffaaa8f06
nishantk444 <kadamnishu55@gmail.com>
2024-08-17 00:29:16 +0530

    if-else_1

---
+++ if-else_1.dart
@@ -0,0 +1,148 @@
+// 1
+// void main(){
+//   var x = 10;
+//   if(x%2==0){
+//     print("${x} is an even no");
+//   }else if(x%2!=0){
+//     print("${x} is an odd no");
+//   }
+// }
+
+
+// 2
+// void main(){
+//   var x = 5;
+//   if(x<=10){
+//     print("${x} is less than 10");
+//   }else if(x>10){
+//     print("${x} is greater than 10");
+//   }
+// }
+
+
+// 3
+// void main(){
+//   var x = 20;
+//   if(x>=18){
+//     print("You can cast a vote");
+//   }else {
+//     print("You cannot cast a vote");
+//   }
+// }
+
+
+// 4
+// void main(){
+//   var x = 20;
+//   if(x<0){
+//     print("${x} is a negative number");
+//   }else if(x>=0){
+//     print("${x} is a positive number");
+//   }
+// }
+
+
+// 5
+// void main(){
+//   var x = 'O';
+//   if(x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U'){
+//     print("${x} is a vowel");
+//   }else{
+//     print("${x} is a consonant");
+//   }
+// }
+
+
+// 6
+// void main(){
+//   var x = 3;
+//   if(x==1){
+//     print("one");
+//   }else if(x==2){
+//     print("two");
+//   }else if(x==3){
+//     print("three");
+//   }else if(x==4){
+//     print("four");
+//   }else if(x==5){
+//     print("five");
+//   }else if(x>5){
+//     print("${x} is greater than 5");
+//   }
+// }
+
+
+// 7
+// void main(){
+//   var x = 38;
+//   if(x==1){
+//     print("January has 31 days");
+//   }else if(x==2){
+//     print("February has 30 days");
+//   }else if(x==3){
+//     print("March has 31 days");
+//   }else if(x==4){
+//     print("April has 30 days");
+//   }else if(x==5){
+//     print("May has 31 days");
+//   }else if(x==6){
+//     print("June has 30 days");
+//   }else if(x==7){
+//     print("July has 31 days");
+//   }else if(x==8){
+//     print("August has 31 days");
+//   }else if(x==9){
+//     print("September has 30 days");
+//   }else if(x==10){
+//     print("October has 31 days");
+//   }else if(x==11){
+//     print("November has 30 days");
+//   }else if(x==12){
+//     print("December has 31 days");
+//   }else{
+//     print("Invalid month");
+//   }
+// }
+
+
+// 8
+// void main(){
+//   var x = 39;
+//   if(x%3==0){
+//     print("Divisible by 3");
+//   }else if(x%5==0){
+//     print("Divisible by 5");
+//   }else if(x%3!=0 && x%5!=0){
+//     print("Not divisible by 3 or 5");
+//   }
+// }
+
+
+// 9
+// void main(){
+//   var x = 39;
+//   if(x==1){
+//     print("please pay 2000 rupees");
+//   }else if(x==2){
+//     print("please pay 3000 rupees");
+//   }else if(x==3){
+//     print("please pay 7000 rupees");
+//   }else{
+//     print("please pay 2500 rupees");
+//   }
+// }
+
+
+// 10
+// void main(){
+//   var x = 152;
+//   if(x<90){
+//     print(x);
+//   }else if(x>=90 || x<180){
+//     print(x*6);
+//   }else if(x>=180 || x<250){
+//     print(x*10);
+//   }else if(x>=250){
+//     print(x*15);
+//   }
+// }
