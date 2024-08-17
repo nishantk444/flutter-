13e4fd7be7dd9f6c711c71052e3c8dda76d509b2
nishantk444 <kadamnishu55@gmail.com>
2024-08-17 23:05:18 +0530

    basic_for_loop

---
+++ basic_for_loop.dart
@@ -0,0 +1,81 @@
+// 1
+// void main(){
+//   for(int i=1; i<=10; i++){
+//     print(i);
+//   }
+// }
+
+// 2
+// void main(){
+//   for(int i=1; i<=100; i++){
+//     print(i);
+//   }
+// }
+
+// 3
+// void main(){
+//   var x = 100;
+//   for(int i=x; i<x+10; i++){
+//     print(i);
+//   }
+// }
+
+// 4
+// void main(){
+//   for(int i=1; i<=100; i++){
+//     if(i%2==0){
+//       print(i);
+//     }
+//   }
+// }
+
+// 5
+// void main(){
+//   for(int i=1; i<=50; i++){
+//     if(i%2==1){
+//       print(i);
+//     }
+//   }
+// }
+
+// 6
+// void main(){
+//   for(int i=100; i>=1; i--){
+//     print(i);
+//   }
+// }
+
+// 7
+// void main(){
+// var x = 12;
+// for(int i=1; i<=10; i++){
+//   print(x*i);
+//   }
+// }
+
+// 8
+// void main() {
+//   var x = 12;
+//   for (int i = 10; i >= 1; i--) {
+//     print(x * i);
+//   }
+// }
+
+// 9
+// void main() {
+//   var x = 0;
+//   for (int i = 1; i <= 10; i++) {
+//     x = i + x;
+//   }
+//   print(x);
+// }
+
+
+// 10
+// void main(){
+//   var x = 1;
+//   for(int i=1; i<=10; i++){
+//     x=i*x;
+//   }
+//   print(x);
+// }
