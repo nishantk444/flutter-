ed3756cd570b6c1eed31da6e125c9cc78481d9bd
nishantk444 <kadamnishu55@gmail.com>
2024-08-18 00:44:44 +0530

    basic_for_loop_2

---
+++ basic_for_loop_2.dart
@@ -0,0 +1,94 @@
+// 1
+// void main(){
+//   for(int i=31; i<=55; i++){
+//     print(i);
+//   }
+// }
+
+// 2
+// void main(){
+//   for(int i=63; i<=123; i++){
+//     if(i%9==0){
+//       print(i)
+//     }
+//   }
+// }
+
+// 3
+// void main() {
+//   var x = 0;
+//   for (int i = 20; i <= 120; i++) {
+//     if (i % 2 == 1) {
+//       x = i + x;
+//     }
+//   }
+//   print(x);
+// }
+
+// 4
+// void main(){
+//   // var x = 2;
+//   for(int i=100; i<=120; i++){
+//     // i=i*i;
+//     print(i*i);
+//   }
+// }
+
+// 5
+// void main() {
+//   for (int i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 4 == 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// 6
+// void main() {
+//   for (int i = 20; i <= 50; i++) {
+//     if (i % 4 == 3) {
+//       print(i);
+//     }
+//   }
+// }
+
+// 7
+// void main(){
+//   for(int i=20; i<=60; i++){
+//     if(i%7==0){
+//       print(i*i*i);
+//     }
+//   }
+// }
+
+// 8
+// void main() {
+//   var x = 12;
+//   int sum = 0;
+//   for (int i = 1; i <= 10; i++) {
+//     sum += x * i;
+//   }
+//   print(sum);
+// }
+
+// 9
+// void main() {
+//   var x = 15;
+//   var sum = 0;
+//   for (int i = 1; i <= x; i++) {
+//     sum += i * i;
+//   }
+//   print(sum);
+// }
+
+
+// 10
+// void main(){
+//   for(int i=20; i<=70; i++){
+//     if(i%2==0){
+//       print(i*i*i);
+//     }else if(i%2==1){
+//       print(i*i);
+//     }
+//   }
+// }
