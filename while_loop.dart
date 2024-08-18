5ac3a55b9144294d85695857dc8a8cfe15411a71
nishantk444 <kadamnishu55@gmail.com>
2024-08-18 16:47:41 +0530

    while_loop

---
+++ while_loop.dart
@@ -0,0 +1,110 @@
+// 1
+// void main() {
+//   int x = 31;
+//   while (x <= 55) {
+//     print(x);
+//     x++;
+//   }
+// }
+
+// 2
+// void main() {
+//   int i = 63;
+//   while (i <= 123) {
+//     if (i % 9 == 0) {
+//       print(i);
+//     }
+//     i++;
+//   }
+// }
+
+// 3
+// void main() {
+//   var x = 0;
+//   int i = 20;
+//   while (i <= 120) {
+//     if (i % 2 == 1) {
+//       x += i;
+//     }
+//     i++;
+//   }
+//   print(x);
+// }
+
+// 4
+// void main() {
+//   int i = 100;
+//   while (i <= 120) {
+//     print(i * i);
+//     i++;
+//   }
+// }
+
+// 5
+// void main() {
+//   int i = 1;
+//   while (i <= 100) {
+//     if (i % 3 == 0 && i % 4 == 0) {
+//       print(i);
+//     }
+//     i++;
+//   }
+// }
+
+// 6
+// void main() {
+//   int i = 20;
+//   while (i <= 50) {
+//     if (i % 4 == 3) {
+//       print(i);
+//     }
+//     i++;
+//   }
+// }
+
+// 7
+// void main() {
+//   int i = 20;
+//   while (i <= 60) {
+//     if (i % 7 == 0) {
+//       print(i * i * i);
+//     }
+//     i++;
+//   }
+// }
+
+// 8
+// void main() {
+//   var sum = 0;
+//   var x = 12;
+//   int i = 1;
+//   while (i <= 10) {
+//     sum += x * i;
+//     i++;
+//   }
+//   print(sum);
+// }
+
+// 9
+// void main() {
+//   var sum = 0;
+//   int i = 1;
+//   while (i <= 15) {
+//     sum += i * i;
+//     i++;
+//   }
+//   print(sum);
+// }
+
+// 10
+// void main() {
+//   int i = 20;
+//   while (i <= 70) {
+//     if (i % 2 == 1) {
+//       print(i * i);
+//     } else if (i % 2 == 0) {
+//       print(i * i * i);
+//     }
+//     i++;
+//   }
+// }
