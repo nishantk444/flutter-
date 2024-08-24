e8a4655adc61495b41cb7fa063395210247d79be
nishantk444 <kadamnishu55@gmail.com>
2024-08-24 21:53:12 +0530

    pattern_1

---
+++ pattern.dart
@@ -0,0 +1,124 @@
+import 'dart:io';
+
+// 1
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   for (int i = 1; i <= n; i++) {
+//     for (int i = 1; i <= n; i++) {
+//       stdout.write("*# ");
+//     }
+//     stdout.writeln();
+//   }
+// }
+
+// 2
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   for (int i = 1; i <= n; i++) {
+//     for (int i = 1; i <= n; i++) {
+//       stdout.write("INC ");
+//     }
+//     stdout.writeln();
+//   }
+// }
+
+// 3
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   for (int i = 1; i <= n; i++) {
+//     for (int i = 1; i <= n; i++) {
+//       stdout.write("$i ");
+//     }
+//     stdout.writeln();
+//   }
+// }
+
+// 4
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write("$i ");
+//     }
+//     stdout.writeln();
+//   }
+// }
+
+// 5
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   for (int i = n; i >= 1; i--) {
+//     for (int j = n; j >= 1; j--) {
+//       stdout.write("$i ");
+//     }
+//     stdout.writeln();
+//   }
+// }
+
+// 6
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   var x = 14;
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write("${x} ");
+//     }
+//     stdout.writeln();
+//     x++;
+//   }
+// }
+
+// 7
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   var x = 1;
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write("$x ");
+//       x += 2;
+//     }
+//     stdout.writeln();
+//   }
+// }
+
+// 8
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   var x = 1;
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write(x);
+//       x++;
+//     }
+//     stdout.writeln();
+//     x -= n - 1;
+//   }
+// }
+
+// 9
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   var x = 1;
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write(x);
+//       x++;
+//     }
+//     stdout.writeln();
+//     x = x - 1;
+//   }
+// }
+
+// 10
+// void main() {
+//   var n = int.parse(stdin.readLineSync()!);
+//   var x = 1;
+//   for (int i = 1; i <= n; i++) {
+//     for (int j = 1; j <= n; j++) {
+//       stdout.write(x);
+//       x++;
+//     }
+//     stdout.writeln();
+//     x -= n - 1;
+//   }
+// }
